4196a1ba3a0a0cfaf6811bd9e91bb63e3d3bd37d
Yanis <janis.sci@gmail.com>
2014-01-12 20:11:11 +0400

    Basic implementation

---
+++ src/classes.js
@@ -0,0 +1,95 @@
+// Object.keys   IE9+
+// [].forEach    IE9+
+
+/*
+
+var Vehicle = Classes.create({
+    fields: ['kpd']
+});
+
+var
+
+ */
+
+(function() {
+
+    // ------ Helper methods -------------
+    function extendObj(obj, ext) {
+        if (!ext) {
+            return obj;
+        }
+
+        var out = { };
+
+        Object.keys(obj).forEach(function(k) {
+            out[k] = obj[k];
+        });
+
+        Object.keys(ext).forEach(function(k) {
+            out[k] = ext[k];
+        });
+
+        return out;
+    }
+
+    function addAccessors(obj, _props, name) {
+        obj[name] = function(value) {
+            if (!value) {
+                return _props[name];
+            } else {
+                _props[name] = value;
+            }
+        }
+    }
+
+    function extendArray(arrOfArrays) {
+        var out = [];
+
+        arrOfArrays.forEach(function(arr) {
+            arr.forEach(function(el) {
+                out.push(el);
+            });
+        });
+
+        return out;
+    }
+
+    var Classes = window.Classes = { };
+
+    Classes.create = function(options) {
+        var clazz = { };
+
+        clazz.create = function(properties) {
+            var obj = { };
+            var _props = extendObj({}, properties);
+            obj._class = clazz;
+
+            options.fields.forEach(function(name) {
+                addAccessors(obj, _props, name);
+            });
+
+            if (options.methods) {
+                Object.keys(options.methods).forEach(function(m) {
+                    obj[m] = function() {
+                        var args = Array.prototype.slice.call(arguments)
+                        options.methods[m].apply(obj, args);
+                    }
+                });
+            }
+
+            return obj;
+        };
+
+        clazz.extend = function(extOptions) {
+            var newOptions = { };
+            newOptions.fields = extendArray(
+                [(options.fields || []), (extOptions.fields || [])]
+            );
+
+            return Classes.create(newOptions);
+        };
+
+        return clazz;
+    };
+
+})();
---
+++ test/classes_spec.js
@@ -0,0 +1,79 @@
+describe("Classes", function () {
+
+    var Pet;
+
+    beforeEach(function () {
+        Pet = Classes.create
+        ({
+            fields: ['kind', 'age', 'name'],
+            methods: {
+                growUp: function() {
+                    this.age(this.age() + 1);
+                },
+
+                changeNameAndAge: function(name, age) {
+                    this.name(name);
+                    this.age(age);
+                }
+            }
+        });
+    });
+
+    describe('contructing a new object', function () {
+
+        it("should assign getters and setters + correctly work with constructor", function () {
+
+            var tom = Pet.create({kind: 'Cat', age: 2, name: 'Tom'});
+
+            expect(tom._class).toBe(Pet);
+
+            expect(tom.kind()).toBe('Cat');
+            expect(tom.age()).toBe(2);
+            expect(tom.name()).toBe('Tom');
+
+            tom.age(23);
+            expect(tom.age()).toBe(23);
+
+        });
+
+        it('should correctly call methods', function() {
+            var tom = Pet.create({kind: 'Cat', name: 'Tom', age: 1});
+
+            expect(tom.age()).toBe(1);
+            tom.growUp();
+            expect(tom.age()).toBe(2);
+            tom.changeNameAndAge('Kitty', 3);
+            expect(tom.name()).toBe('Kitty');
+            expect(tom.age()).toBe(3);
+
+        });
+
+        xit('should reject assigning not defined properties', function() {
+            // todo: pending
+        });
+
+    });
+
+    describe('extending a class', function() {
+
+        it('should extend a class', function() {
+
+            var HomyPet = Pet.extend({
+                fields: ['address']
+            });
+
+            var tom = HomyPet.create({kind: 'Cat', age: 2, name: 'Tom', address: 'New-York'});
+
+            expect(tom._class).toBe(HomyPet);
+
+            expect(tom.kind()).toBe('Cat');
+            expect(tom.age()).toBe(2);
+            expect(tom.name()).toBe('Tom');
+            expect(tom.address()).toBe('New-York');
+
+        });
+
+    });
+
+
+});
